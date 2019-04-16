83ead374a651da81b74a7dcabc0b724f98a153d6
Hieu <hieu@thanhhieu>
2019-04-16 20:34:06 +0700

    Xong 6 Cau

--- Cau5.m
+++ Cau5.m
@@ -1,5 +1,7 @@
 M = [3 -1 1; -1 4 0; 1 0 5];
+
 x = [1 2 -1];
+
 y = [-2 0 1];
 % transposed matrix : ma tran chuyen vi
 dx = sqrt(x * M * transpose(x))
--- Cau6.m
+++ Cau6.m
@@ -1,15 +1,8 @@
-syms a b c;
+% Cau 6
+E = [1 2 3; 1 3 4; -2 -5 -6];
 
-%a*[1; 1; -2] + b*[2; -3; 5] + c*[3; 4; -6] == [2; 1; -4]; is what
-% we write in paper
+fE = [1 2 3; 2 5 7; -2 -3 -5];
 
-pt1 = a + 2*b + 3*c == 2;
-pt2 = a - 3*b + 4*c == 1;
-pt3 = -2*a + 5*b -6*c == -4;
+M = fE * inv(E);
 
-[A, B] = equationsToMatrix([pt1, pt2, pt3] , [a, b, c]);
-X = linsolve(A,B)
-
-M = [1 2 -2; 2 5 -3; 3 7 -5];
-
-% calculate the required f
+f_cantim = M * [2; 1; -4]
